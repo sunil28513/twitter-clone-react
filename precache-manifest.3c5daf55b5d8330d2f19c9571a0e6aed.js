d097835df65759ad983c7aebbbab1c9979822d1d
Verma <v.nishad@slnsoftwares.com>
2020-09-01 19:35:28 +0530

    Updates

---
+++ precache-manifest.3c5daf55b5d8330d2f19c9571a0e6aed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194bbe7de3b24e3c1d7a73f7df867428",
+    "url": "/twitter-clone-react/%20/index.html"
+  },
+  {
+    "revision": "369468eaa2e902a83c44",
+    "url": "/twitter-clone-react/%20/static/css/main.ab3128cf.chunk.css"
+  },
+  {
+    "revision": "3582be870a5b76898d5d",
+    "url": "/twitter-clone-react/%20/static/js/2.f16d9232.chunk.js"
+  },
+  {
+    "revision": "a8de4128b679d6159aad8871b5569c6d",
+    "url": "/twitter-clone-react/%20/static/js/2.f16d9232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "369468eaa2e902a83c44",
+    "url": "/twitter-clone-react/%20/static/js/main.90eba232.chunk.js"
+  },
+  {
+    "revision": "150c9e033004f329c217",
+    "url": "/twitter-clone-react/%20/static/js/runtime-main.fb52874d.js"
+  }
+]);
